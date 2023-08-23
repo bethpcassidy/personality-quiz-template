9e4f28d53f8ad6a10c9ef02afe448484bf20a9ec
beth <bethcassdy@gmail.com>
2023-08-23 17:20:32 -0400

    cards and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { React } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,50 +10,52 @@ function App() {
   const [showResponse, setResponse] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [startQuiz, setQuiz] = useState(false);
+
   const questions = [
     {
       text: "What season were you born?",
       options: [
-        { id: 0, text: "Fall", isCorrect: false },
-        { id: 1, text: "Summer", isCorrect: false },
-        { id: 2, text: "Winter", isCorrect: false },
-        { id: 3, text: "Spring", isCorrect: true },
+        { id: 0, text: "Fall" },
+        { id: 1, text: "Summer" },
+        { id: 2, text: "Winter" },
+        { id: 3, text: "Spring" },
       ],
     },
     {
       text: "Which food do you like the most?",
       options: [
-        { id: 0, text: "Pasta", isCorrect: true },
-        { id: 1, text: "Soup", isCorrect: false },
-        { id: 2, text: "Stir Fry", isCorrect: false },
-        { id: 3, text: "Salad", isCorrect: false },
+        { id: 0, text: "Pasta" },
+        { id: 1, text: "Soup" },
+        { id: 2, text: "Stir Fry" },
+        { id: 3, text: "Salad" },
       ],
     },
     {
       text: "What are you most likely to find in your pockets.",
       options: [
-        { id: 0, text: "Old Tickets", isCorrect: true },
-        { id: 1, text: "Pen", isCorrect: false },
-        { id: 2, text: "Laniard", isCorrect: false },
-        { id: 3, text: "Benjamin Franklin", isCorrect: false },
+        { id: 0, text: "Old Tickets" },
+        { id: 1, text: "Pen" },
+        { id: 2, text: "Laniard" },
+        { id: 3, text: "Benjamin Franklin" },
       ],
     },
     {
       text: "It's raining, you:",
       options: [
-        { id: 0, text: "Read a book.", isCorrect: false },
-        { id: 1, text: "Watch a show.", isCorrect: true },
-        { id: 2, text: "Play cards.", isCorrect: false },
-        { id: 3, text: "Go out for a walk.", isCorrect: false },
+        { id: 0, text: "Read a book." },
+        { id: 1, text: "Watch a show." },
+        { id: 2, text: "Play cards." },
+        { id: 3, text: "Go out for a walk." },
       ],
     },
     {
       text: "What book title would you grab first.",
       options: [
-        { id: 0, text: "Gardening Journal: Corinthians and You", isCorrect: false },
-        { id: 1, text: "Mystery on 47th Avenue", isCorrect: true },
-        { id: 2, text: "Photos From Beach Towns Around the World", isCorrect: true },
-        { id: 3, text: "The History of Snakes", isCorrect: false },
+        { id: 0, text: "Gardening Journal: Corinthians and You" },
+        { id: 1, text: "Mystery on 47th Avenue" },
+        { id: 2, text: "Photos From Beach Towns Around the World" },
+        { id: 3, text: "The History of Snakes" },
       ],
     },
   ];
@@ -84,7 +87,7 @@ function App() {
     },
   ];
 
-  const [finalResult, setFinalResult] = useState([]);
+  const [finalResult, setFinalResult] = useState(false);
 
   //Function
   const optionClicked = (id) => {
@@ -112,33 +115,52 @@ function App() {
     <>
       <div className="App">
         {/* //header */}
-        <h1>Survey</h1>
-        {/* //status */}
-        {/* <h2>test score {score}</h2> */}
-        {showResponse ? (
-          <div className="feedback">
-            <h1>You Got:{finalResult.text} </h1>
-            <h2>{finalResult.text}</h2>
-            <button>Honk</button>
+        <div className="top">
+          <h1>Survey</h1>
+        </div>
+        {startQuiz ? (
+          <div className="content">
+            {/* //status */}
+            {/* <h2>test score {score}</h2> */}
+            {showResponse ? (
+              <div className="feedback">
+                <div class="card">
+                  <img className="image" src="https://i.imgflip.com/6ek11y.png?a469848" alt="Card image cap" />
+                  <h1>You Got:{finalResult.text} </h1>
+                  <div class="card-body">
+                    <h2>{finalResult.text}</h2>
+                  </div>
+                  <button>Honk</button>
+                </div>
+              </div>
+            ) : (
+              <div class="card">
+                <div className="Questions">
+                  <h2>
+                    {" "}
+                    {currentQuestion + 1} of {questions.length}{" "}
+                  </h2>
+                  <div>
+                    <h3 className="question-text"> {questions[currentQuestion].text} </h3>
+                    <ul>
+                      {questions[currentQuestion].options.map((option) => {
+                        return (
+                          <li onClick={() => optionClicked(option.id)} key={option.id}>
+                            {option.text}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         ) : (
-          <div className="Questions">
-            <h2>
-              {" "}
-              {currentQuestion + 1} of {questions.length}{" "}
-            </h2>
-            <div>
-              <h3 className="question-text"> {questions[currentQuestion].text} </h3>
-              <ul>
-                {questions[currentQuestion].options.map((option) => {
-                  return (
-                    <li onClick={() => optionClicked(option.id)} key={option.id}>
-                      {option.text}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+          <div class="card">
+            <h1 className="start" onClick={() => setQuiz(true)}>
+              Start Quiz
+            </h1>
           </div>
         )}
       </div>
